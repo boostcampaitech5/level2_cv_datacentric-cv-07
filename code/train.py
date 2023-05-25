04917a291a28d1eb7f4dc0a476827159eeed8505
TwinKay <twinkay@yonsei.ac.kr>
2023-05-25 03:17:05 +0000

    feat: best pth 파일 저장 코드 추가 및 경로 수정

--- code/train.py
+++ code/train.py
@@ -11,13 +11,15 @@ import random
 import wandb
 from torch import cuda
 from torch.utils.data import DataLoader
-from torch.utils.data.dataset import random_split
 from torch.optim import lr_scheduler
 from tqdm import tqdm
 
 from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
+from early_stopping import EarlyStopping
+
+wandb_name = 'test'
 
 def seed_everything(seed):
     torch.manual_seed(seed)
@@ -35,23 +37,18 @@ def parse_args():
     parser = ArgumentParser()
 
     # Conventional args
-    parser.add_argument('--data_dir', type=str,
-                        default=os.environ.get('SM_CHANNEL_TRAIN', '../data/medical'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',
-                                                                        'trained_models'))
-
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '../data/medical'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',f'trained_models/{wandb_name}'))
     parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
     parser.add_argument('--num_workers', type=int, default=8)
-
     parser.add_argument('--image_size', type=int, default=2048)
     parser.add_argument('--input_size', type=int, default=1024)
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--max_epoch', type=int, default=150)
-    parser.add_argument('--save_interval', type=int, default=5)
+    parser.add_argument('--save_interval', type=int, default=1)
     parser.add_argument('--ignore_tags', type=list, default=['masked', 'excluded-region', 'maintable', 'stamp'])
     parser.add_argument('--seed', type=int, default=42)
-
     args = parser.parse_args()
 
     if args.input_size % 32 != 0:
@@ -61,12 +58,12 @@ def parse_args():
 
 def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval, ignore_tags, seed):
+
     seed_everything(seed)
-    
 
     train_dataset = SceneTextDataset(
         data_dir,
-        split='train1',
+        split='train0',
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags
@@ -74,15 +71,14 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     
     val_dataset = SceneTextDataset(
         data_dir,
-        split='val1',
+        split='val0',
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags
     )
+
     train_dataset = EASTDataset(train_dataset)
     val_dataset = EASTDataset(val_dataset)
-    
-    # train_dataset, val_dataset = random_split(dataset, [80,20])
     num_batches = math.ceil(len(train_dataset) / batch_size) 
 
     train_loader = DataLoader(
@@ -106,9 +102,10 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    early_stopping = EarlyStopping(patience=10, delta=0.0, mode='min', verbose=False)
 
     best_val_loss = np.inf
-    wandb.init(entity="oif", project='Data_Centric', name='test')
+    wandb.init(entity="oif", project='Data_Centric', name=wandb_name)
     for epoch in range(max_epoch):
         train_start = time.time()
         model.train()
@@ -140,7 +137,7 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
                 pbar.set_postfix(train_dict) # 진행 바 오른쪽에 Train Loss 설명 추가
 
                 mean_train_loss = train_loss / num_batches
-
+                 
                 # wandb 로그 기록
                 step = epoch * num_batches + pbar.n
                 log_wandb(step, train_loss, train_dict)
@@ -159,13 +156,26 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
                 if best_val_loss > mean_val_loss:
                     best_val_loss = mean_val_loss
                     best_val_loss_epoch = epoch+1
+                    if not osp.exists(model_dir):
+                        os.makedirs(model_dir)
+                    
+                    ckpt_fpath = osp.join(model_dir, f'best_{epoch+1}epoch.pth')
+                    torch.save(model.state_dict(), ckpt_fpath)
+                    if osp.exists(ckpt_fpath):
+                        prev_ckpt_fpath = osp.join(model_dir, f"best_{epoch}epoch.pth")
+                        if osp.exists(prev_ckpt_fpath):
+                            torch.save(model.state_dict(), prev_ckpt_fpath)
+                            os.remove(prev_ckpt_fpath)
 
-                print(f'(Val) Class loss={extra_info1["cls_loss"]}, (Val) Angle loss={extra_info["angle_loss"]}, (Val) IoU loss={extra_info["iou_loss"]}')
+                print(f'(Val) Class loss: {extra_info["cls_loss"]:.4f}, (Val) Angle loss: {extra_info["angle_loss"]:.4f}, (Val) IoU loss: {extra_info["iou_loss"]:.4f}')
 
             print('(Train) Mean loss: {:.4f} | Elapsed time: {}'.format(mean_train_loss, timedelta(seconds=time.time() - train_start)))
             print('(Val)   Mean loss: {:.4f} | Elapsed time: {}'.format(mean_val_loss, timedelta(seconds=time.time() - val_start)))
             print('Best Validation Loss: {:.4f} at Epoch {}'.format(best_val_loss, best_val_loss_epoch))
-            
+
+            early_stopping(mean_val_loss)
+            if early_stopping.early_stop:
+                raise SystemExit(0)
 
         scheduler.step()
         
@@ -175,6 +185,7 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
         mean_loss_iou = epoch_loss_iou / num_batches
 
         log_dict = {
+            "mean_val_loss": mean_val_loss,
             "mean_loss": mean_loss,
             "mean_loss_cls": mean_loss_cls,
             "mean_loss_angle": mean_loss_angle,
